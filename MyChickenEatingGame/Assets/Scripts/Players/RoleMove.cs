a4dac038312f0b8861a7e15d5a16e5773fadfd8c
zhanghuanfu <459205672@qq.com>
2018-07-31 14:53:29 +0800

    Add Role move

---
+++ MyChickenEatingGame/Assets/Scripts/Players/RoleMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoleMove : MonoBehaviour {
+
+    Animator _animator;
+	// Use this for initialization
+	void Start () {
+        _animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var hor = Input.GetAxis("Horizontal");
+        var ver = Input.GetAxis("Vertical");
+
+        _animator.SetFloat("SpeedX", hor);
+        _animator.SetFloat("SpeedZ", ver);
+ 	}
+}
