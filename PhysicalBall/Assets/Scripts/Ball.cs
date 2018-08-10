90c222e77c9a6d09b637cbdbb68e80141a85e09a
zhanghuanfu <459205672@qq.com>
2018-08-10 15:55:04 +0800

    Physic Ball Game Begin

---
+++ PhysicalBall/Assets/Scripts/Ball.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float ForceSize = 10;
+    Rigidbody2D rigidBall;
+    public Transform EmitPosition;
+
+	// Use this for initialization
+	void Start () {
+        rigidBall = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        EmitBall();
+
+	}
+
+    void EmitBall()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = mousePos - transform.position;
+            direction.Normalize();
+
+            rigidBall.bodyType = RigidbodyType2D.Dynamic;
+            rigidBall.AddForce(direction * ForceSize, ForceMode2D.Impulse);
+        }
+    }
+
+    public void ResetBall()
+    {
+        transform.position = EmitPosition.position;
+
+        //rigidBall.velocity = Vector2.zero;
+        //rigidBall.angularVelocity = 0;
+        //rigidBall.mass = 0;
+        rigidBall.bodyType = RigidbodyType2D.Static;
+    }
+}
---
+++ PhysicalBall/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Obstacle : MonoBehaviour {
+
+    public int Hp = 10;
+    public Text HpText;
+	// Use this for initialization
+	void Start () {
+        HpText.text = Hp.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Hp -= 1;
+        HpText.text = Hp.ToString();
+    }
+}
